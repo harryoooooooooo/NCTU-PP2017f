d41959f1fa608873ec40fc641d553474c2d3eb3d
harryoooooooooo <ymsc27884@gmail.com>
2018-01-09 23:32:04 +0800

    add arg

--- systest.sh
+++ systest.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/fish
 
 for i in ( seq $argv[1] )
-    ./testdata_gen $argv[2] 100 | tee td$i | time ./brute_force | tee out$i > /dev/null
+    ./testdata_gen $argv[2] $argv[3] | tee td$i | time ./brute_force | tee out$i > /dev/null
     if not ./check td$i out$i
         exit 1
     end
+    sleep 1
 end
