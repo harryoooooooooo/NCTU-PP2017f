ff6e645e45f7ec042abf3ce8235851b3cb799ff9
harryoooooooooo <ymsc27884@gmail.com>
2018-01-09 17:58:52 +0800

    test script nug

--- systest.sh
+++ systest.sh
@@ -2,7 +2,7 @@
 
 for i in ( seq $argv[1] )
     ./testdata_gen $argv[2] 100 | tee td$i | time ./brute_force | tee out$i > /dev/null
-    if not ./check td out
+    if not ./check td$i out$i
         exit 1
     end
 end
