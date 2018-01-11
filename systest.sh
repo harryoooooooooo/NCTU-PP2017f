52258c1f0ffcac3841a2ccecc4be1c45c271e1ab
harryoooooooooo <ymsc27884@gmail.com>
2018-01-11 13:57:55 +0800

    configable

--- systest.sh
+++ systest.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/fish
 
+set EF "./a.out"
+set OUT "out"
+set TD "td"
+
 for i in ( seq $argv[1] )
-    ./testdata_gen $argv[2] $argv[3] | tee td$i | time ./brute_force | tee out$i > /dev/null
-    if not ./check td$i out$i
+    if [ $argv[2] = "old" ]
+        cat $TD$i | time $EF | tee $OUT$i > /dev/null
+    else
+        ./testdata_gen $argv[2] $argv[3] | tee $TD$i | time $EF | tee $OUT$i > /dev/null
+    end
+    if not ./check $TD$i $OUT$i
         exit 1
     end
     sleep 1
